1077b0c80bd28fb99aadaf52f8c6db23dcfd8377
Karan <karan.2005iitjee2023@gmail.com>
2025-05-21 00:45:29 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  const shopNowButton = document.querySelector("button");
+  shopNowButton.addEventListener("click", () => {
+    alert("Redirecting to Shop Page... (You can replace this with actual redirection)");
+    // Example: window.location.href = "shop.html";
+  });
+});
